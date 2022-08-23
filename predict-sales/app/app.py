d7224863372bf2eb6fd5c8e8084c57c5cacb8eea
chaika <dbrugger@redhat.com>
2022-08-23 10:00:29 -0400

    updated version

--- predict-sales/app/app.py
+++ predict-sales/app/app.py
@@ -40,8 +40,8 @@ def results():
 
     output = round(prediction[0], 2)
     return jsonify(
-        prediction=output*1.3,
-        message="From Version 1.3")
+        prediction=output*1.4,
+        message="From Version 1.4")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
