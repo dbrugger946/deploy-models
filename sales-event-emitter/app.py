190b519f5af0c9de77e0471cd57d3d2524d925a5
chaika <sea5@bird888.com>
2023-09-21 15:24:50 -0400

    update for predict app

--- sales-event-emitter/app.py
+++ sales-event-emitter/app.py
@@ -9,84 +9,29 @@ import uuid
 from kafka import KafkaProducer
 
 
-CE_TEMPLATE = { 
-  "specversion": "1.0", 
-  "id": "a89b61a2-5644-487a-8a86-144855c5dce8", 
-  "source": "SomeEventSource", 
-  "type": "DecisionRequest", 
-  "subject": "TheSubject", 
-  "kogitodmnmodelname": "Personalization", 
-  "kogitodmnmodelnamespace": "https://kiegroup.org/dmn/_746FB059-0238-4996-9009-28DB94C559AF", 
-  "data": {
-    "Current Event": {
-      "CustomerName": "John",
-      "Category": "CC_TRANSACTION",
-      "EventDate": "2021-10-19T19:24:46.559Z",
-      "EventID": "",
-      "EventSource": "",
-      "Value": "AIRLINE_PURCHASE",
-      "ResponseType": "",
-      "ResponseMessage": ""
-    }
-  } 
-}
-
-t_dict = {"data": {
-    "Current Event": {
-      "CustomerName": "James",
-      "Category": "CC_BALANCE_PAYMENT",
-      "EventDate": "2021-10-19T19:24:46.559Z",
-      "EventID": "",
-      "EventSource": "",
-      "Value": "MIN_DUE",
-      "ResponseType": "",
-      "ResponseMessage": ""
-    }
-  } }
-# CE_TEMPLATE["data"] = t_dict
 
 
-EVENT_TEMPLATES = [
-     { "Current Event": { "CustomerName": "James", "Category": "CC_BALANCE_PAYMENT", "EventDate": "","EventID": "","EventSource": "CUSTOMERCARE", "Value": "LATE_PAYMENT" ,"ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "CC_BALANCE_PAYMENT", "EventDate": "","EventID": "","EventSource": "MOBLE", "Value": "MIN_DUE","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "CC_BALANCE_PAYMENT", "EventDate": "","EventID": "","EventSource": "WEBSITE", "Value": "MIN_DUE","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "CC_TRANSACTION", "EventDate": "","EventID": "","EventSource": "WEBSITE", "Value": "AIRLINE_PURCHASE","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "CC_TRANSACTION", "EventDate": "","EventID": "","EventSource": "POS", "Value": "MERCHANT_PURCHASE","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "CC_TRANSACTION", "EventDate": "","EventID": "","EventSource": "POS", "Value": "HOTEL_PURCHASE","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "CC_TRANSACTION", "EventDate": "","EventID": "","EventSource": "WEBSITE", "Value": "ONLINE_PURCHASE","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "DISPUTES", "EventDate": "","EventID": "","EventSource": "IVR", "Value": "CASE_CREATED","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "DISPUTES", "EventDate": "","EventID": "","EventSource": "IVR", "Value": "CASE_CLOSED","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "ONLINE_ACCOUNT", "EventDate": "","EventID": "","EventSource": "CUSTOMERCARE", "Value": "PAYMENT_FAILURE","ResponseType": "", "ResponseMessage": ""}},
-     { "Current Event": { "CustomerName": "James", "Category": "ONLINE_ACCOUNT", "EventDate": "","EventID": "","EventSource": "CUSTOMERCARE", "Value": "PAYMENT_SUCCESS","ResponseType": "", "ResponseMessage": ""}}
+
+
+SALES_INPUT = [
+    {"rate":5, "sales_in_first_month":200, "sales_in_second_month":400},
+    {"rate":5, "sales_in_first_month":200, "sales_in_second_month":400},
+    {"rate":5, "sales_in_first_month":200, "sales_in_second_month":400},
+    {"rate":5, "sales_in_first_month":200, "sales_in_second_month":400},
+    {"rate":5, "sales_in_first_month":200, "sales_in_second_month":400}
 ]
 
 
 
-ATM_EVENT = [
-    { "eventCategory": "ATM_WITHDRAWAL", "eventValue": "Geo-US", "eventSource": "ATM"}
-
-]
-
-
-
-CUSTOMER = [
-
-    'John',
-    'James'
-]
 
 def generate_event():
-    CE_TEMPLATE["data"] = EVENT_TEMPLATES[random.randint(0, 10)]
+    INPUTS = SALES_INPUT[random.randint(0, 4)]
 
-    CE_TEMPLATE["data"]["Current Event"]["CustomerName"] = CUSTOMER[random.randint(0, 1)]
-
-    ret = CE_TEMPLATE
+    ret = INPUTS
     return ret
 
 
-def generate_event_atm():
-    ret = ATM_EVENT[0]
-    return ret
+
 
 
 def main(args):
@@ -105,8 +50,8 @@ def main(args):
 #        producer.send("atm-withdrawal", json.dumps(generate_event_atm()).encode(), json.dumps(CUSTOMER[0]).encode())
 #        logging.info(json.dumps(generate_event_atm()).encode())
 
-        producer.send("sales", json.dumps(generate_event()).encode(),'{"rate":5, "sales_in_first_month":200, "sales_in_second_month":400}' )
-        logging.info('{"rate":5, "sales_in_first_month":200, "sales_in_second_month":400}')
+        producer.send(args.topic, json.dumps(generate_event()).encode(),1)
+        logging.info(json.dumps(generate_event()))
         
         time.sleep(args.rate)
     logging.info('end sending events')
@@ -133,16 +78,16 @@ if __name__ == '__main__':
     parser.add_argument(
         '--brokers',
         help='The bootstrap servers, env variable KAFKA_BROKERS',
-        default='my-cluster-kafka-bootstrap:9092')
+        default='my-cluster-kafka-bootstrap.user1.svc:9092')
     parser.add_argument(
         '--topic',
         help='Topic to publish to, env variable KAFKA_TOPIC',
-        default='event-input-stream')
+        default='sales-info')
     parser.add_argument(
         '--rate',
         type=int,
         help='Lines per second, env variable RATE',
-        default=5)
+        default=1)
     args = parse_args(parser)
     main(args)
     logging.info('exiting')
